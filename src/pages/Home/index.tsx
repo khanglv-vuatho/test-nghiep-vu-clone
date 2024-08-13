f63d9de6cf41c75a8f2fd6133be606ca9b880565
khanglv-vuatho <khanglv@vuatho.com>
2024-08-13 09:14:40 +0700

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -32,6 +32,7 @@ export default function Home() {
         </div>
       )}
       <div className={`px-6 ${isNotStep2End ? 'h-full' : 'h-[calc(100dvh-56px)] p-4 pb-2'}`}>
+        123
         <Suspense fallback={<Fallback />}>{steps[currentStep]}</Suspense>
       </div>
     </DefaultLayout>
